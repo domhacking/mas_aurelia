a15a556c9f1c7dca7c861608063de215fdc40c37
Dominic Hacking <dominichacking@gmail.com>
2015-08-10 08:33:52 +0100

    Completed first draft of homepage

--- source/js/module/nav/nav.js
+++ source/js/module/nav/nav.js
@@ -5,10 +5,56 @@
  */
 
 
-var Module = require('../abstract-module');
+var Module 	= require('../abstract-module');
+var page 	  = require('page');
+var $       = require('jquery');
 
 module.exports = Module.extend({
 
-  template: require('./nav.html')
+  	template: require('./nav.html'),
+
+  	oninit: function() {
+  		this.on('goToHome', this.onGoHome);
+  		this.on('goToGallery', this.onGoGallery);
+  		this.on('goToInfo', this.onGoInfo);
+  		this.on('goToLocalArea', this.onGoLocalArea);
+  		this.on('goToContact', this.onGoContact);
+    },
+
+
+    onGoHome: function(){
+        page('/');
+    },
+
+	onGoGallery: function(){
+        page('/gallery');
+    },
+
+    onGoInfo: function(){
+        page('/info');
+    },
+
+	onGoLocalArea: function(){
+        page('/Localarea');
+    },
+
+	onGoContact: function(){
+        page('/contact');
+    }
 
 });
+
+
+// var mn = $(".nav");
+//     mns = "main-nav-scrolled";
+//     hdr = $('homescreen').height();
+
+// $(window).scroll(function() {
+//     if( $(this).scrollTop() > hdr ) {
+//         mn.addClass(mns);
+//         console.log('test');
+//     } else {
+//         mn.removeClass(mns);
+//     }
+// });
+
--- source/js/view/main.js
+++ source/js/view/main.js
@@ -1,26 +1,59 @@
 var Ractive = require('ractive');
+var page    = require('page');
 Ractive.components = require('../module');
 
 module.exports = function() {
 
-  return new Ractive({
+    return new Ractive({
 
-    el: document.querySelector('.main'),
+        el: document.querySelector('.main'),
 
-    template: require('./main.html'),
+        template: require('./main.html'),
 
-    data: {
-    },
+        data: {
+        },
 
-    computed: {
-    },
+        computed: {
+        },
 
-    oninit: function() {
-    },
+        oninit: function() {
+            this.setRouter();
+        },
 
-    onrender: function() {
-    }
+        onrender: function() {
+        },
 
-  });
+        setRouter: function(){
+            var self = this;
+
+
+            page('/', function () {
+                self.set('view', 'home');
+            });
+
+            page('/gallery', function () {
+                self.set('view', 'gallery');
+            });
+
+            page('/info', function () {
+                self.set('view', 'info');
+            });
+
+            page('/localarea', function () {
+                self.set('view', 'localArea');
+            });
+
+            page('/contact', function () {
+                self.set('view', 'contact');
+            });
+
+            page({
+                click: false,
+                dispatch: true,
+                hashbang: false,
+            });
+        }
+
+    });
 
 };
