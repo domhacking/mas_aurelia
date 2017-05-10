600be583afbb43ab1b46cbfe94f3e9070a758d48
Dominic Hacking <dominic.hacking@havas.com>
2017-05-10 22:05:07 +0100

    Website pushed live with map working, form working and image resized:

--- source/js/config.js
+++ source/js/config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-  api: 'AIzaSyDafTlC4jzLb2Wo2PRA6asRCqsqR6yETxo'
+  api: 'AIzaSyAagPKWBcmd0NHxBS94-ARzzQrskR0AhoI'
+  // api: 'AIzaSyDafTlC4jzLb2Wo2PRA6asRCqsqR6yETxo'
   // api: 'AIzaSyATQWQ5SdqcMIM6GMIsItohPZZyb8TGtrE'
 
 }
